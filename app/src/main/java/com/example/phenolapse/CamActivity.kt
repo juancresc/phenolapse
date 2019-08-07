ce0fde2d0b208282507a35cf29f50305e48800a0
Juan Manuel Crescente <you@example.com>
2019-08-07 14:33:33 -0300

    asd

---
+++ app/src/main/java/com/example/phenolapse/CamActivity.kt
@@ -0,0 +1,140 @@
+package com.example.phenolapse
+
+import java.util.*
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
+import android.os.Build
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v4.content.FileProvider
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+
+
+import com.androidhiddencamera.CameraConfig
+import com.androidhiddencamera.CameraError
+import com.androidhiddencamera.HiddenCameraActivity
+import com.androidhiddencamera.HiddenCameraUtils
+import com.androidhiddencamera.config.CameraFacing
+import com.androidhiddencamera.config.CameraFocus
+import com.androidhiddencamera.config.CameraImageFormat
+import com.androidhiddencamera.config.CameraResolution
+import com.androidhiddencamera.config.CameraRotation
+
+class CamActivity : HiddenCameraActivity() {
+    private val REQ_CODE_CAMERA_PERMISSION = 1253
+
+    private var mCameraConfig: CameraConfig? = null
+
+    private lateinit var imageView: ImageView
+    private var photoFile: File? = null
+    private val CAPTURE_IMAGE_REQUEST = 1
+    private lateinit var mCurrentPhotoPath: String
+    private val IMAGE_DIRECTORY_NAME = "VLEMONN"
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        imageView = findViewById(R.id.image_last)
+
+        val btn_click_me = findViewById(R.id.btn_agregar_alarma) as Button
+
+        btn_click_me.setOnClickListener {
+            val newFragment = TimePickerFragment()
+            newFragment.show(fragmentManager, "TimePicker")
+
+
+        }
+
+
+        val btn_captura = findViewById(R.id.btn_captura) as Button
+
+
+
+        var mCameraConfig = CameraConfig()
+            .getBuilder(this)
+            .setCameraFacing(CameraFacing.REAR_FACING_CAMERA)
+            .setCameraResolution(CameraResolution.HIGH_RESOLUTION)
+            .setImageFormat(CameraImageFormat.FORMAT_JPEG)
+            .setImageRotation(CameraRotation.ROTATION_270)
+            .setCameraFocus(CameraFocus.AUTO)
+            .build()
+
+
+        //Check for the camera permission for the runtime
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+
+            //Start camera preview
+            startCamera(mCameraConfig)
+        } else {
+            ActivityCompat.requestPermissions(
+                this, arrayOf(Manifest.permission.CAMERA),
+                REQ_CODE_CAMERA_PERMISSION
+            )
+        }
+
+        //Take a picture
+        btn_captura.setOnClickListener(View.OnClickListener {
+            //Take picture using the camera without preview.
+            Toast.makeText(this, "hola charola", Toast.LENGTH_LONG).show()
+            takePicture()
+        })
+
+    }
+    override fun onImageCapture(imageFile: File) {
+
+        // Convert file to bitmap.
+        // Do something.
+        val options = BitmapFactory.Options()
+        options.inPreferredConfig = Bitmap.Config.RGB_565
+        val bitmap = BitmapFactory.decodeFile(imageFile.absolutePath, options)
+
+        //Display the image to the image view
+        (findViewById(R.id.image_last) as ImageView).setImageBitmap(bitmap)
+    }
+
+    override fun onCameraError(@CameraError.CameraErrorCodes errorCode: Int) {
+        when (errorCode) {
+            CameraError.ERROR_CAMERA_OPEN_FAILED ->
+                //Camera open failed. Probably because another application
+                //is using the camera
+                Toast.makeText(this, R.string.error_cannot_open, Toast.LENGTH_LONG).show()
+            CameraError.ERROR_IMAGE_WRITE_FAILED ->
+                //Image write failed. Please check if you have provided WRITE_EXTERNAL_STORAGE permission
+                Toast.makeText(this, R.string.error_cannot_write, Toast.LENGTH_LONG).show()
+            CameraError.ERROR_CAMERA_PERMISSION_NOT_AVAILABLE ->
+                //camera permission is not available
+                //Ask for the camera permission before initializing it.
+                Toast.makeText(this, R.string.error_cannot_get_permission, Toast.LENGTH_LONG).show()
+            CameraError.ERROR_DOES_NOT_HAVE_OVERDRAW_PERMISSION ->
+                //Display information dialog to the user with steps to grant "Draw over other app"
+                //permission for the app.
+                HiddenCameraUtils.openDrawOverPermissionSetting(this)
+            CameraError.ERROR_DOES_NOT_HAVE_FRONT_CAMERA -> Toast.makeText(
+                this,
+                R.string.error_not_having_camera,
+                Toast.LENGTH_LONG
+            ).show()
+        }
+    }
+
+
+}
--- app/src/main/java/com/example/phenolapse/MainActivity.kt
+++ app/src/main/java/com/example/phenolapse/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.phenolapse
 
 import java.util.*
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.os.Build
@@ -17,6 +19,7 @@ import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
 import android.support.v4.content.FileProvider
 import android.util.Log
+import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
@@ -24,7 +27,21 @@ import java.io.File
 import java.io.IOException
 import java.text.SimpleDateFormat
 
+
+import com.androidhiddencamera.CameraConfig
+import com.androidhiddencamera.CameraError
+import com.androidhiddencamera.HiddenCameraActivity
+import com.androidhiddencamera.HiddenCameraUtils
+import com.androidhiddencamera.config.CameraFacing
+import com.androidhiddencamera.config.CameraFocus
+import com.androidhiddencamera.config.CameraImageFormat
+import com.androidhiddencamera.config.CameraResolution
+import com.androidhiddencamera.config.CameraRotation
+
 class MainActivity : AppCompatActivity() {
+    private val REQ_CODE_CAMERA_PERMISSION = 1253
+
+    private var mCameraConfig: CameraConfig? = null
 
     private lateinit var imageView: ImageView
     private var photoFile: File? = null
@@ -35,157 +52,11 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        imageView = findViewById(R.id.image_last)
+        //setContentView(R.layout.activity_main)
 
-        val btn_click_me = findViewById(R.id.btn_agregar_alarma) as Button
+        val intent = Intent(this,CamActivity::class.java)
+        startActivity(intent)
 
-        btn_click_me.setOnClickListener {
-            val newFragment = TimePickerFragment()
-            newFragment.show(fragmentManager, "TimePicker")
-
-
-        }
-        val btn_captura = findViewById(R.id.btn_captura) as Button
-
-        btn_captura.setOnClickListener {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                captureImage()
-            } else {
-                captureImage2()
-            }
-        }
-
-    }
-
-
-    /* Capture Image function for 4.4.4 and lower. Not tested for Android Version 3 and 2 */
-    private fun captureImage2() {
-
-        try {
-            val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            photoFile = createImageFile4()
-            if (photoFile != null) {
-                displayMessage(baseContext, photoFile!!.getAbsolutePath())
-                Log.i("Mayank", photoFile!!.getAbsolutePath())
-                val photoURI = Uri.fromFile(photoFile)
-                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-                startActivityForResult(cameraIntent, CAPTURE_IMAGE_REQUEST)
-            }
-        } catch (e: Exception) {
-            displayMessage(baseContext, "Camera is not available." + e.toString())
-        }
-
-    }
-
-    private fun captureImage() {
-
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE), 0)
-        } else {
-            val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            if (takePictureIntent.resolveActivity(packageManager) != null) {
-                // Create the File where the photo should go
-                try {
-
-                    photoFile = createImageFile()
-                    displayMessage(baseContext, photoFile!!.getAbsolutePath())
-                    Log.i("Mayank", photoFile!!.getAbsolutePath())
-
-                    // Continue only if the File was successfully created
-                    if (photoFile != null) {
-
-
-
-                        var photoURI = FileProvider.getUriForFile(this,
-                            "com.example.phenolapse.fileprovider",
-                            photoFile?:return)
-
-                        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
-
-                        startActivityForResult(takePictureIntent, CAPTURE_IMAGE_REQUEST)
-
-                    }
-                } catch (ex: Exception) {
-                    // Error occurred while creating the File
-                    Log.d("---<",ex.message.toString())
-                    displayMessage(baseContext,"Capture Image Bug: "  + ex.message.toString())
-                }
-
-
-            } else {
-                displayMessage(baseContext, "Nullll")
-            }
-        }
-
-
-    }
-    override fun onActivityResult(requestCode:Int, resultCode:Int, data:Intent?)
-    {
-
-
-    //Bundle extras = data.getExtras();
-        //Bitmap imageBitmap = (Bitmap) extras.get("data");
-        //imageView.setImageBitmap(imageBitmap);
-
-        if (requestCode == CAPTURE_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
-            val myBitmap = BitmapFactory.decodeFile(photoFile!!.getAbsolutePath())
-            imageView.setImageBitmap(myBitmap)
-        } else {
-            displayMessage(baseContext, "Request cancelled or something went wrong.")
-        }
-    }
-
-    private fun createImageFile4(): File? {
-        // External sdcard location
-        val mediaStorageDir = File(
-            Environment
-                .getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),
-            IMAGE_DIRECTORY_NAME)
-        // Create the storage directory if it does not exist
-        if (!mediaStorageDir.exists()) {
-            if (!mediaStorageDir.mkdirs()) {
-                displayMessage(baseContext, "Unable to create directory.")
-                return null
-            }
-        }
-
-        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss",
-            Locale.getDefault()).format(Date())
-
-        return File(mediaStorageDir.path + File.separator
-                + "IMG_" + timeStamp + ".jpg")
-
-    }
-
-    @Throws(IOException::class)
-    private fun createImageFile(): File {
-        // Create an image file name
-        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
-        val imageFileName = "JPEG_" + timeStamp + "_"
-        val storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES)
-        val image = File.createTempFile(
-            imageFileName, /* prefix */
-            ".jpg", /* suffix */
-            storageDir      /* directory */
-        )
-
-        // Save a file: path for use with ACTION_VIEW intents
-        mCurrentPhotoPath = image.absolutePath
-        return image
-    }
-
-    private fun displayMessage(context: Context, message: String) {
-        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        if (requestCode == 0) {
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED
-                && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
-                captureImage()
-            }
-        }
 
     }
 }
